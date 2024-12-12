eb36e045ff979d8d12e4b71fc58348627e9a1f8e
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-12-12 01:17:17 -0500

    Create CarModelAdmin class in 'admin.py'

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -9,6 +9,10 @@ admin.site.register(CarModel)
 # CarModelInline class
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id', 'seating_capacity', 'number_of_doors', 'transmission', 'fuel', 'mileage', 'engine_size')
+    list_filter = ('car_make', 'type', 'year', 'fuel')
+    search_fields = ('name', 'car_make_name', 'type', 'transmission', 'fuel')
 
 # CarMakeAdmin class with CarModelInline
 
