5e8790488439a469171ea055338eb869a3e22f50
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-12-12 14:08:08 -0500

    Add path for 'get_dealers' view in '/djangoapp/urls.py'

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,6 +18,10 @@ urlpatterns = [
     # path for get_cars view
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
+    # path for get dealers view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
     # path for dealer reviews view
 
     # path for add a review view
