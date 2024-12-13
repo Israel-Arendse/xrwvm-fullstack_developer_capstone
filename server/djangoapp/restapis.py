c83c9abdab62a25cac324e4223b5fb9c65b43d03
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-12-12 23:40:17 -0500

    Create 'post_review' method in 'restapis.py'

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -41,5 +41,12 @@ def analyze_review_sentiments(text):
         print(f"Unexcepted {err=}, {type(err)=}")
         print("Network exception occurred")
 
-# def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    # Add code for posting review
+    request_url = backend_url+"/insert_review"
+    try:
+        response = request.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network execption occurred")
