d5f54c0a2863699ebced8b68eb647ae3d841f749
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-12-17 16:19:36 +0400

    Update admin.py (Improved admin file for linting)

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,15 +6,17 @@ It includes custom admin classes to display and manage car makes and models.
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # CarModelInline class
 # pylint: disable=too-few-public-methods
 class CarModelInline(admin.TabularInline):
-    """ 
-    Inline admin descriptor for CarModel objects, to be used in the CarMakeAdmin. 
-    This allows CarModel objects to be edited inline within the CarMake admin page. 
+    """
+    Inline admin descriptor for CarModel objects, to be used in the CarMakeAdmin.
+    This allows CarModel objects to be edited inline within the CarMake admin page.
     """
     model = CarModel
-    extra = 1 # Number of extra forms to display in the admin interface
+    extra = 1  # Number of extra forms to display in the admin interface
+
 
 # CarModelAdmin class
 # pylint: disable=too-few-public-methods
@@ -27,11 +29,12 @@ class CarModelAdmin(admin.ModelAdmin):
         'name', 'car_make', 'type', 'year', 'dealer_id', 'seating_capacity',
         'number_of_doors', 'transmission', 'fuel', 'mileage', 'engine_size'
     )
-    list_filter = ('car_make', 'type', 'year', 'fuel') # Filters for easy data sorting
+    list_filter = ('car_make', 'type', 'year', 'fuel')  # Filters for easy data sorting
     search_fields = (
-        'name', 'car_make__name', 'type', 
+        'name', 'car_make__name', 'type',
         'transmission', 'fuel'
-    ) # Fields to search through in the admin interface
+    )  # Fields to search through in the admin interface
+
 
 # CarMakeAdmin class with CarModelInline
 # pylint: disable=too-few-public-methods
@@ -41,8 +44,9 @@ class CarMakeAdmin(admin.ModelAdmin):
     search fields, and inline editing of related CarModel objects within the Django admin interface.
     """
     list_display = ('name', 'description', 'website')
-    search_fields = ('name', 'description') # Fields to search through in the admin interface
-    inlines = [CarModelInline] # Allows CarModel objects to be edited inline within CarMake
+    search_fields = ('name', 'description')  # Fields to search through in the admin interface
+    inlines = [CarModelInline]  # Allows CarModel objects to be edited inline within CarMake
+
 
 # Register models here
 
