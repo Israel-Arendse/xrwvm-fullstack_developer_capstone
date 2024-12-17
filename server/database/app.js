dfd4468c670fa36c3405caf83221b76b2455815d
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-12-17 22:17:59 +0400

    Update app.js (Add missing semicolons to line 4, 5, 8, and 63)

--- server/database/app.js
+++ server/database/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -60,7 +60,7 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
   try {
-    const documents = await Dealerships.find()
+    const documents = await Dealerships.find();
     res.json(documents);
   } catch (error) {
     res.status(500).json({error: 'Error fetching documents'});
