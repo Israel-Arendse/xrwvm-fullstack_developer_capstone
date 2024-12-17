6b4b864d94636b8f251d040065b13abe671be68d
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-12-17 20:26:33 +0400

    Update models.py (Add two blank lines to models)

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,6 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 # CarMake model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -12,6 +13,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
 
 # CarModel model
 class CarModel(models.Model):
@@ -27,7 +29,12 @@ class CarModel(models.Model):
         ('HATCHBACK', 'Hatchback'),
         ('HYBRID', 'Hybrid'),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    type = models.CharField
+    (
+        max_length=10, 
+        choices=CAR_TYPES, 
+        default='SUV'
+    )
     year = models.IntegerField(
         default=2023,
         validators=[
