8e9aa7cef4c919441b066e119fa84d333ff2e08a
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-12-17 18:21:26 +0400

    Update admin.py (Add blank lines)

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
 """
-This module registers the CarMake and 
+This module registers the CarMake and
 CarModel models with the Django admin interface.
 It includes custom admin classes to display
 and manage car makes and models.
@@ -8,9 +8,10 @@ and manage car makes and models.
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 class CarModelInline(admin.TabularInline):
     """
-    Inline admin descriptor for CarModel objects, 
+    Inline admin descriptor for CarModel objects,
     to be used in the CarMakeAdmin.
     This allows CarModel objects to be edited 
     inline within the CarMake admin page.
@@ -18,10 +19,13 @@ class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1  # Number of extra forms to display in the admin interface
 
+
 class CarModelAdmin(admin.ModelAdmin):
     """
-    Admin descriptor for CarModel objects, defining the fields to display in the list view,
-    filters for easy data sorting, and search fields within the Django admin interface.
+    Admin descriptor for CarModel objects, 
+    defining the fields to display in the list view,
+    filters for easy data sorting, 
+    and search fields within the Django admin interface.
     """
     list_display = (
         'name', 'car_make', 'type', 'year', 'dealer_id', 'seating_capacity',
@@ -35,10 +39,14 @@ class CarModelAdmin(admin.ModelAdmin):
         'transmission', 'fuel'
     )  # Fields to search through in the admin interface
 
+
 class CarMakeAdmin(admin.ModelAdmin):
     """
-    Admin descriptors for CarMake objects, defining the fields to display in the list view,
-    search fields, and inline editing of related CarModel objects within the Django admin interface.
+    Admin descriptors for CarMake objects, 
+    defining the fields to display in the list view,
+    search fields, and inline editing of 
+    related CarModel objects within
+    the Django admin interface.
     """
     list_display = ('name', 'description', 'website')
     search_fields = (
@@ -49,3 +57,4 @@ class CarMakeAdmin(admin.ModelAdmin):
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
+
